bbdbcf795d357a5e43bd46ea54cd5a11b8ffddb4
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-31 20:31:01 +0300

    Configuring Widgets & Understanding Objects

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 63, 5, 120),
         body: Center(
           child: Text('Hello World!'),
         ),
