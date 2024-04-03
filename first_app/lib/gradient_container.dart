bccc816614ec7afbd045435c0cdb6c2417883b22
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-04-03 17:26:39 +0300

    How NOT To Build Interactive Widgets

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -4,17 +4,19 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2, {super.key});
+  GradientContainer(this.color1, this.color2, {super.key});
 
-  const GradientContainer.purple({super.key})
+  GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
+  var activeDiceImage = "assets/images/dice-2.png";
 
   rollDice() {
-    // ...
+    activeDiceImage = "assets/images/dice-4.png";
+    print("Changing dice image to $activeDiceImage");
   }
 
   @override
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,11 +4,11 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         body: GradientContainer(
-          Color.fromARGB(255, 33, 5, 109),
-          Color.fromARGB(255, 68, 21, 149),
+          const Color.fromARGB(255, 33, 5, 109),
+          const Color.fromARGB(255, 68, 21, 149),
         ),
         // body: GradientContainer(
         //   [
