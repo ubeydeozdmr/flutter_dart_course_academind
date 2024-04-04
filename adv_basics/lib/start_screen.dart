bc1cf38cdd84bd256fe87f157c2b5d9f8b503de0
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-04-04 17:28:16 +0300

    Adding Transparency to Widgets

--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -12,7 +13,15 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             "assets/images/quiz-logo.png",
             width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset(
+          //     "assets/images/quiz-logo.png",
+          //     width: 300,
+          //   ),
+          // ),
           const SizedBox(height: 80),
           const Text(
             "Learn Flutter the fun way!",
