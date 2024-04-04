caae8af9c62e79a9450a0caa50a8360d0d26d094
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-04-04 15:32:28 +0300

    Some sketch for challenge

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,1 +1,69 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Quiz App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Flutter Quiz App'),
+      // ),
+      // backgroundColor: Colors.deepPurpleAccent,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepPurpleAccent, Colors.deepPurple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 200,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                ),
+              ),
+              const SizedBox(height: 20),
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.deepPurple,
+                textColor: Colors.white,
+                child: const Text('Start Quiz'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
