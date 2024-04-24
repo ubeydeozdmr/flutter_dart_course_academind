169f3a69f8a43a704428a9a1a425642780684975
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-04-24 23:23:23 +0300

    Using Third-Party Packages & Adding Google Fonts

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
@@ -37,8 +38,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(
-                color: Colors.white,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
