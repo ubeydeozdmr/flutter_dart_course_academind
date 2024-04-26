779cfc1758d29501e05e584ff222af6cbb256219
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-04-26 11:38:22 +0300

    Expanded To The Rescue!

--- adv_basics/lib/questions_summary.dart
+++ adv_basics/lib/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
@@ -8,21 +9,25 @@ class QuestionsSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: summaryData.map((data) {
-        return Row(
-          children: [
-            Text(((data['question_index'] as int) + 1).toString()),
-            Column(
-              children: [
-                Text(data['question'] as String),
-                const SizedBox(height: 5),
-                Text(data['correct_answer'] as String),
-                Text(data['user_answer'] as String),
-              ],
-            )
-          ],
-        );
-      }).toList(),
+      children: summaryData.map(
+        (data) {
+          return Row(
+            children: [
+              Text(((data['question_index'] as int) + 1).toString()),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(data['question'] as String),
+                    const SizedBox(height: 5),
+                    Text(data['correct_answer'] as String),
+                    Text(data['user_answer'] as String),
+                  ],
+                ),
+              )
+            ],
+          );
+        },
+      ).toList(),
     );
   }
 }
