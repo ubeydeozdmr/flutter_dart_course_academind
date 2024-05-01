14bd40a13ed58a7de47f0bd0197f69e945de70f6
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-05-01 22:28:01 +0300

    Setting Text Themes

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -12,10 +12,29 @@ void main() {
       theme: ThemeData().copyWith(
         // useMaterial3: true,
         colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
+        appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
         ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.primaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
       ),
       home: const Expenses(),
     ),
