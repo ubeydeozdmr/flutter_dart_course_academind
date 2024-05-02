4edb3964f0e8910fa282b1b4b7c1641e72a0e9c6
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-05-02 18:35:24 +0300

    Mutating Values in Memory & Making Sense of var, final & const

--- flutter_internals/lib/main.dart
+++ flutter_internals/lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_internals/keys/keys.dart';
 
 void main() {
+  final numbers = [1, 2, 3];
+  // numbers = [4, 5, 6]; // Error: A value of type 'List<int>' can't be assigned to a variable of type 'List<int>'.
+  numbers.add(4);
+
   runApp(const App());
 }
 
