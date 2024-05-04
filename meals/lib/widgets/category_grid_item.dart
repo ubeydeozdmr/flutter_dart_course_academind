2e56ddb80ae87a9ec0fa685eba8397309e00732c
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-05-04 17:12:08 +0300

    chore: Update category grid item styling

--- meals/lib/widgets/category_grid_item.dart
+++ meals/lib/widgets/category_grid_item.dart
@@ -19,16 +19,16 @@ class CategoryGridItem extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            gradient: LinearGradient(
-              colors: [
-                category.color.withOpacity(0.55),
-                category.color.withOpacity(0.9),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.circular(15)),
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
         child: Text(
           category.title,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
