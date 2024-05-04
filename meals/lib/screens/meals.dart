42ed85fae3b17df193333248d6ed5521923445d1
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-05-04 17:44:36 +0300

    Loading Meals Data Into a Screen

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -3,32 +3,52 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
 
   final String title;
   final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(
+          meals[index].title,
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Scrollable(
-        viewportBuilder: (context, position) {
-          return Column(
-            children: [
-              if (meals.isEmpty) const Text('No meals found'),
-              for (final meal in meals)
-                ListTile(
-                  title: Text(meal.title),
-                  subtitle: Text(meal.categories.join(', ')),
-                  trailing: Text('\$${meal.steps.length * 10}'),
-                ),
-            ],
-          );
-        },
-      ),
+      body: content,
     );
   }
 }
