9a0f1f413bddad631e36d40cfdbc248fbdb30aa5
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-05-12 00:12:37 +0300

    Getting Started with Another Provider

--- meals/lib/screens/tabs.dart
+++ meals/lib/screens/tabs.dart
@@ -7,6 +7,7 @@ import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
 import 'package:meals/providers/meals_provider.dart';
 import 'package:meals/providers/favorites_provider.dart';
+import 'package:meals/providers/filters_provider.dart';
 
 const kInitialFilters = {
   Filter.glutenFree: false,
