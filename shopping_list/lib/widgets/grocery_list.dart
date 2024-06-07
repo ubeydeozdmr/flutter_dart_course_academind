1f9287bdaebf952da7fb7aca517e8a75b7152183
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-06-07 21:25:33 +0300

    Final Challenge Solution

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -29,41 +29,39 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  Widget _emptyList() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('No groceries yet.'),
-          const SizedBox(height: 12),
-          ElevatedButton(
-            onPressed: _addItem,
-            child: const Text('Add one'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _groceryList() {
-    return ListView.builder(
-      itemCount: _groceryItems.length,
-      itemBuilder: (ctx, index) => ListTile(
-        title: Text(_groceryItems[index].name),
-        leading: Container(
-          width: 24,
-          height: 24,
-          color: _groceryItems[index].category.color,
-        ),
-        trailing: Text(
-          _groceryItems[index].quantity.toString(),
-        ),
-      ),
-    );
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(child: Text('No items added yet.'));
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
@@ -74,7 +72,7 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: _groceryItems.isEmpty ? _emptyList() : _groceryList(),
+      body: content,
     );
   }
 }
