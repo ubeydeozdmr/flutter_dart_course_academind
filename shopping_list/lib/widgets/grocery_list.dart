c573dea207483784184f5934c8955842c7b646a0
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-06-09 01:02:32 +0300

    Sending DELETE Requests

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -74,10 +74,24 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final url = Uri.https(
+        'flutter-prep-80a3a-default-rtdb.europe-west1.firebasedatabase.app', 'shopping-list/${item.id}.json');
+
+    await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      // Optional: Show error message
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
+
   }
 
   @override
