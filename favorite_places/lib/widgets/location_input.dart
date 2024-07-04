13c0bd6e97753a571d1916fef1e08d72b372a963
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-07-04 17:34:44 +0300

    Using the Picked Location in the Form

--- favorite_places/lib/widgets/location_input.dart
+++ favorite_places/lib/widgets/location_input.dart
@@ -9,7 +9,9 @@ import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/constants.dart';
 
 class LocationInput extends StatefulWidget {
-  const LocationInput({super.key});
+  const LocationInput({super.key, required this.onSelectLocation});
+
+  final void Function(PlaceLocation location) onSelectLocation;
 
   @override
   State<LocationInput> createState() {
@@ -80,6 +82,8 @@ class _LocationInputState extends State<LocationInput> {
       );
       _isGettingLocation = false;
     });
+
+    widget.onSelectLocation(_pickedLocation!);
   }
 
   @override
