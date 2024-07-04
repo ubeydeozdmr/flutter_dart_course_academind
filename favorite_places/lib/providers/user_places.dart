802392de1c6f75db6d1b24498f5deb87bb643106
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-07-05 01:09:04 +0300

    Storing Place Data in a (On-Device) SQL Database

--- favorite_places/lib/providers/user_places.dart
+++ favorite_places/lib/providers/user_places.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:favorite_places/models/place.dart';
 
@@ -16,6 +18,30 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 
     final newPlace =
         Place(title: title, image: copiedImage, location: location);
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)',
+        );
+      },
+      version: 1,
+    );
+    db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'loc_lat': newPlace.location.latitude,
+        'loc_lng': newPlace.location.longitude,
+        'address': newPlace.location.address,
+      },
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+
     state = [newPlace, ...state];
   }
 }
