4d6f8d7fbe8e1d08ca86694e974c2edd1ffde527
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-07-10 00:49:20 +0300

    Formatting fix

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -7,43 +7,41 @@ class ChatMessages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('chat')
-            .orderBy(
-              'createdAt',
-              descending: false,
-            )
-            .snapshots(),
-        builder: (ctx, chatSnapshots) {
-          if (chatSnapshots.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
-            return const Center(
-              child: Text('No messages found.'),
-            );
-          }
-
-          if (chatSnapshots.hasError) {
-            return const Center(
-              child: Text('Something went wrong...'),
-            );
-          }
-
-          final loadedMessages = chatSnapshots.data!.docs;
-
-          return ListView.builder(
-            itemCount: loadedMessages.length,
-            itemBuilder: (ctx, index) => Text(
-              loadedMessages[index].data()['text'],
-            ),
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy(
+            'createdAt',
+            descending: false,
+          )
+          .snapshots(),
+      builder: (ctx, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
           );
-        });
-    return const Center(
-      child: Text('No messages found.'),
+        }
+
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages found.'),
+          );
+        }
+
+        if (chatSnapshots.hasError) {
+          return const Center(
+            child: Text('Something went wrong...'),
+          );
+        }
+
+        final loadedMessages = chatSnapshots.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (ctx, index) => Text(
+            loadedMessages[index].data()['text'],
+          ),
+        );
+      },
     );
   }
 }
